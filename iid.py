965125071ae4bc2b40f5e18656d279386690d3f9
joanna350 <joanna@kaist.ac.kr>
2019-01-21 22:48:28 +0000

    Add files via upload

---
+++ iid.py
@@ -0,0 +1,88 @@
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+from nltk.stem import PorterStemmer
+
+
+class dailybatch():
+    'inverted index based on words from caption,tags from API + text'
+    def __init__():
+        self.iid ={}
+        self.stem = PorterStemmer()
+
+    def load(self, link):
+    'later replace with path, and take as parameter to process multiple files'
+        with open('toydata.json') as tdata:
+            red = json.load(tdata)
+        "json file will be read in dictionary
+         per API results at least: 
+        metadata = size, filetype of picture
+        description -> preprocess 'captions', forget about 'tags'(too many, no numeral)"
+        return red 
+
+    def collect_label(self, jsondic):
+        "tags: cut above the confidence 50%
+        key of list of dictionaries with 'confidence' and 'name'"
+
+        "hypothetical tweetID"
+        tweetID =jsondic['requestID']
+        for item in jsondic['tags']:
+            if item['confidence']>0.5:
+                key= item['name']
+                if key not in self.iid:
+                    self.iid[key] = [tweetID]
+                else: #given each tweetID will be unique
+                    self.iid[key].append(tweetID)
+
+        caption = jsondic['description']['captions']['text']
+        for term in filter(None, self.tokenize(caption):
+            term = self.stemming(term)
+            if self.rid_stopword(term):
+                if key not in self.iid:
+                    self.iid[key] = [tweetID]
+                else:
+                    self.iid[key].append(tweetID)
+
+
+    def stemming(self,term):
+    'run the Porerstemmer'
+        return self.stem.stem(term)
+
+    def tokenize(self,content):
+    'tokenize on non-alphabetical character'
+        return re.split(r'\W+', content)
+
+    def stopwords(self):
+    'the list of stop words from the link will be used to reduce the number of words we keep in our index'
+        f = urllib.request.urlopen('http://members.unine.ch/jacques.savoy/clef/englishST.txt') 
+        text_to_parse = f.read().decode('utf-8') 
+        self.words = set(filter(None, re.split(r'\r\n', text_to_parse)))
+
+    def rid_stopword(self, word):
+    'criteria to check whether we keep the word or not in the index'
+        if word in self.words:
+            return False
+        return True
+
+
+    def main(self, direc):
+        self.stopwords() #creates words to filter out 
+        #given multiple files in direc
+        for each in direc:
+            ds = self.load()
+            self.collect_label(ds)
+            
+    def update(self, _tweets):
+        #self.iid
+        #how to get realtime tweets
+
+        new_ds = self.load(_tweets)
+        self.collect_label(new_ds)
+
+
+if __name__ == '__main__':
+    directory ='typein_repo_address'
+    db = dailybatch()
+    db.main(directory)
+    print(db.iid)
+#to send to in-memory DB, fill in the git connected to Heroku
